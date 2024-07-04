8bfed4e2f778ff225855f71bc4ec1bf460d76f91
Georgii Lysenko <lysenkogeorgii@gmail.com>
2024-07-04 22:47:24 +0300

    Moved netrw disable to neo-tree config

--- lua/options.lua
+++ lua/options.lua
@@ -5,6 +5,3 @@ vim.opt.shiftwidth = 4
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
-
-vim.g.loaded_netrwPlugin = 1
-vim.g.loaded_netrw = 1
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,16 +1,19 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-    },
-    config = function()
-        vim.keymap.set('n', '<leader>e', ':Neotree toggle <CR>')
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<leader>e", ":Neotree toggle <CR>")
 
-        require("neo-tree").setup({
-            close_if_last_window = true,
-        })
-    end
+		vim.g.loaded_netrwPlugin = 1
+		vim.g.loaded_netrw = 1
+
+		require("neo-tree").setup({
+			close_if_last_window = true,
+		})
+	end,
 }
