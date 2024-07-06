b2671864badb793689f73605a38a7842316985eb
George Lysenko <lysenkogeorgii@gmail.com>
2024-07-06 10:46:41 +0300

    Added lsp-signature plugin

---
+++ lua/plugins/lsp-signature.lua
@@ -0,0 +1,10 @@
+return {
+    'ray-x/lsp_signature.nvim',
+    config = function()
+        require("lsp_signature").setup({
+            handler_opts = {
+                border = "none"
+            }
+        })
+    end
+}
