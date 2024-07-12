9c16a9dcca2db3b6b94ed5925daa991ed5cd8005
George Lysenko <lysenkogeorgii@gmail.com>
2024-07-12 23:07:18 +0300

    Changed path_display for telescope, improved telescope config overall.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,38 +1,35 @@
 return {
-    {
-        "nvim-telescope/telescope.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            {
-                'nvim-telescope/telescope-fzf-native.nvim',
-                build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
-                cond = function()
-                    return vim.fn.executable 'make' == 1
-                end,
-            },
-        },
-        config = function()
-            local builtin = require("telescope.builtin")
-
-            pcall(require('telescope').load_extension, 'fzf')
-
-            vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
-            vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
-            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {})
-        end,
-    },
-    {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
         "nvim-telescope/telescope-ui-select.nvim",
-        config = function()
-            require("telescope").setup({
-                extensions = {
-                    ["ui-select"] = {
-                        require("telescope.themes").get_dropdown({}),
-                    },
-                },
-            })
-
-            require("telescope").load_extension("ui-select")
-        end,
+        {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release',
+            cond = function()
+                return vim.fn.executable 'make' == 1
+            end,
+        },
     },
+    config = function()
+        pcall(require('telescope').load_extension, 'fzf')
+        pcall(require("telescope").load_extension, "ui-select")
+
+        require("telescope").setup({
+            defaults = {
+                path_display = { "smart" }
+            },
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown({}),
+                },
+            },
+        })
+
+        local builtin = require("telescope.builtin")
+
+        vim.keymap.set("n", "<leader>sf", builtin.find_files, {})
+        vim.keymap.set("n", "<leader>sg", builtin.live_grep, {})
+        vim.keymap.set("n", "<leader>sd", builtin.diagnostics, {})
+    end,
 }
